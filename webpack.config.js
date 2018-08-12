ccde7d5602728567f1e5700a5c45ba942388f140
fannyvieira <fanny.vieira@ccc.ufcg.edu.br>
2018-08-12 10:04:50 -0300

    Add initial react structure

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+        output: {
+            path: path.resolve(__dirname, 'dist'),
+            filename: 'bundle.[hash].js',
+        },
+        module: {
+            rules: [
+                { test: /\.css$/,
+                      use: [
+                      { loader: 'style-loader' },
+                      { loader: 'css-loader' }
+                      ]
+                },
+                {
+                  test: /\.js$/,
+                  exclude: /node_modules/,
+                  use: ['babel-loader']
+                }, {
+                  test: /\.jsx?$/,
+                  exclude: /node_modules/,
+                  use: ['babel-loader']
+                }
+            ]
+        },
+        devServer: {
+            contentBase: './dist'
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                template: './public/index.html'
+            })
+        ]
+}
