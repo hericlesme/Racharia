332e85b3f574fbb4290e111747c5508a56d792e6
fannyvieira <fanny.vieira@ccc.ufcg.edu.br>
2018-08-13 18:57:52 -0300

    switch to es6 sintax

--- client/webpack.config.babel.js
+++ client/webpack.config.babel.js
@@ -1,39 +1,41 @@
 import path from 'path';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 
-module.exports = {
-        entry: './src/index.js',
-        output: {
-            path: path.resolve(__dirname, 'dist'),
-            filename: 'bundle.[hash].js',
-        },
-        module: {
-            rules: [
-                { test: /\.css$/,
-                      use: [
-                          { loader: 'style-loader' },
-                          { loader: 'css-loader' }
-                      ]
-                },
-                {
-                  test: /\.js$/,
-                  exclude: /node_modules/,
-                  use: ['babel-loader']
-                }, {
-                  test: /\.jsx?$/,
-                  exclude: /node_modules/,
-                  use: ['babel-loader']
-                }
-            ]
-        },
-        devServer: {
-            contentBase: './dist'
-        },
-        plugins: [
-            new HtmlWebpackPlugin({
-                template: './public/index.html',
-                favicon: './public/favicon.ico'
-            })
-        ],
-        mode: 'development'
+const configuration = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.[hash].js',
+    },
+    module: {
+        rules: [
+            { test: /\.css$/,
+                use: [
+                    { loader: 'style-loader' },
+                    { loader: 'css-loader' }
+                ]
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: ['babel-loader']
+            }, {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                use: ['babel-loader']
+            }
+        ]
+    },
+    devServer: {
+        contentBase: path.resolve(__dirname, 'dist')
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './public/index.html',
+            favicon: './public/favicon.ico'
+        })
+    ],
+    mode: 'development'
 }
+
+export default configuration;
