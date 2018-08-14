759b8ffcd8aeabf8386d5bd521db690eef36ee99
Juan Barros <juanlyrabarros@gmail.com>
2018-08-14 01:50:10 -0300

    Setting config to load json #19

--- node/src/index.js
+++ node/src/index.js
@@ -1,5 +1,5 @@
 import app from './config/server.js';
 
-let server = app.listen(process.env.PORT || 8080, function(){
+let server = app.listen(app['config'].get('app:port'), () => {
 	console.log('Servidor online');
 });
