b184e66ede81d877ca6bcce2163cb9bc989231d4
Juan Barros <juanlyrabarros@gmail.com>
2018-08-25 23:35:21 -0300

    Add unit tests

--- README.md
+++ README.md
@@ -22,3 +22,9 @@ npm run build
 ```
 npm run lint
 ```
+
+### Run your unit tests
+```
+npm run test:unit
+```
+
--- src/components/App.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-const App = () => (<h1> Hello World! :D</h1>);
-
-export default App;
