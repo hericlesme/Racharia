b4c920d97b61a71445c20958c2a24ab6829fab65
fannyvieira <fanny.vieira@ccc.ufcg.edu.br>
2018-08-29 20:11:18 -0300

    added CurrentStep component

---
+++ src/components/step/CurrentStep.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        {{ message }}
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'CurrentStep',
+  data () {
+    return {
+      'message': 'initial structure'
+    }
+  }
+}
+</script>
