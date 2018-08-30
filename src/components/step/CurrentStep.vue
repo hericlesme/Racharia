b3bb40f3e1ff25726e4527e7506b08180f1b5c66
fannyvieira <fanny.vieira@ccc.ufcg.edu.br>
2018-08-29 23:13:10 -0300

    add UsersStep component

--- src/components/step/CurrentStep.vue
+++ src/components/step/CurrentStep.vue
@@ -1,24 +1,32 @@
 <template>
   <div>
     <price-step :updatePrice= updatePrice />
+     <users-step :updateUser= updateUser />
+     {{user}}
   </div>
 </template>
 
 <script>
 import PriceStep from '@/components/step/PriceStep'
+import UsersStep from '@/components/step/UsersStep'
 export default {
   name: 'CurrentStep',
   components: {
-    'price-step': PriceStep
+    'price-step': PriceStep,
+    'users-step': UsersStep
   },
   data () {
     return {
-      price: 0
+      price: 0,
+      user: {}
     }
   },
   methods: {
     updatePrice (price) {
       this.price = price
+    },
+    updateUser (user) {
+      this.user = user
     }
   }
 }
