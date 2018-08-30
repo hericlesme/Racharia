1bb45a06e80a4c16aa508cb8f23b95f0ed06b781
fannyvieira <fanny.vieira@ccc.ufcg.edu.br>
2018-08-29 22:59:10 -0300

    create UsersStep component

---
+++ src/components/step/UsersStep.vue
@@ -0,0 +1,44 @@
+<template>
+    <div id="transfer-panel">
+        <el-transfer
+            v-model="selectedUser"
+            :data="data"
+            :titles="['Opções', 'Selecionado']">
+        </el-transfer>
+    </div>
+</template>
+<script>
+export default {
+  data () {
+    const users = [
+      {
+        key: 1,
+        label: 'Fanny Vieira'
+      }, {
+        key: 2,
+        label: 'Juan Barros'
+      }, {
+        key: 3,
+        label: 'Thayanne Luiza'
+      }
+    ]
+    return {
+      data: users,
+      selectedUser: [],
+    }
+  },
+  watch:{
+      selectedUser(option){
+          const user = this.data.filter(user => {return user.key === option[0]})
+          console.log(user)
+      }
+  }
+}
+</script>
+<style>
+#transfer-panel{
+    text-align: left; 
+    display: inline-block;
+    margin: 50px 0 20px;
+}
+</style>
