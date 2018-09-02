5a0d401b94f33dd3fa6230030a8878c1e258c23b
Thayanne <thayannevls@gmail.com>
2018-09-02 20:35:00 -0300

    init header and footer structure

---
+++ src/components/Footer.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="footer">
+        <p>@OpenDevUFCG</p>
+    </div>
+</template>
+
+<style scoped>
+.footer {
+    position: fixed;
+    left: 0;
+    bottom: 0;
+    width: 100%;
+    background-color: #ffae00;
+    color: white;
+}
+
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,48 @@
+<template>
+<div class="header">
+  <a :href="Home" class="logo">Racharia</a>
+  <div class="header-right">
+    <a class="active" :href="home">Home</a>
+    <a :href="About">About</a>
+  </div>
+</div>
+</template>
+
+<style scoped>
+.header {
+  overflow: hidden;
+  background-color: #ffae00;
+  padding: 5px;
+}
+
+.header a {
+  float: left;
+  color: black;
+  text-align: center;
+  padding: 12px;
+  text-decoration: none;
+  font-size: 18px;
+  line-height: 25px;
+  border-radius: 4px;
+}
+
+.header a.logo {
+  font-size: 25px;
+  font-weight: bold;
+}
+
+.header a:hover {
+  background-color: #ddd;
+  color: black;
+}
+
+.header a.active {
+  background-color: dodgerblue;
+  color: white;
+}
+
+.header-right {
+  float: right;
+}
+
+</style>
